fef681336353b557a13127327b5a5deafad9568f
User <User@UserPC>
2017-04-10 17:58:39 +0900

    4/10 남선우

---
+++ YOLO/src/main/java/com/one/yolo/admin/model/UpfileDAOMybatis.java
@@ -0,0 +1,10 @@
+package com.one.yolo.admin.model;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class UpfileDAOMybatis extends SqlSessionDaoSupport implements UpfileDAO{
+
+}
