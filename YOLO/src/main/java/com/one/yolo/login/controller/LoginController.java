899be140ec9c896c95555a5fea4e2394ec053c3a
User <User@UserPC>
2017-04-10 19:09:58 +0900

    인서트 작업

--- YOLO/src/main/java/com/one/yolo/login/controller/LoginController.java
+++ YOLO/src/main/java/com/one/yolo/login/controller/LoginController.java
@@ -2,14 +2,27 @@ package com.one.yolo.login.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.one.yolo.member.model.MemberService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 	private static final Logger logger
 	=LoggerFactory.getLogger(LoginController.class);
+
 	
+	@RequestMapping(value="/login.do",method=RequestMethod.GET)
+	public String login_get(){
+		logger.info("로그인 화면 보여주기");
+		
+		return "login/login";
+	}
+
 	
 }
---
+++ YOLO/src/main/java/com/one/yolo/member/model/MemberServiceImpl.java
@@ -0,0 +1,5 @@
+package com.one.yolo.member.model;
+
+public class MemberServiceImpl {
+
+}
