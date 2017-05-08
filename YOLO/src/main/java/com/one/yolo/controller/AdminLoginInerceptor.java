9672f2dd9c57fd24d81668ce63fea6a49e9fb2ad
User <User@UserPC>
2017-05-08 11:46:15 +0900

    이창호 관리자막기

--- YOLO/src/main/java/com/one/yolo/controller/AdminLoginInerceptor.java
+++ YOLO/src/main/java/com/one/yolo/controller/AdminLoginInerceptor.java
@@ -36,7 +36,7 @@ public class AdminLoginInerceptor extends HandlerInterceptorAdapter{
 		
 		//관리자 로그인 페이지는 제외시키기
 		String uri = request.getRequestURI();
-		if(uri.indexOf("/admin/login/login.do")!=-1){
+		if(uri.indexOf("/admin/adminLogin.do")!=-1){
 			return true;
 		}
 		
@@ -48,7 +48,7 @@ public class AdminLoginInerceptor extends HandlerInterceptorAdapter{
 			out.println("<script>");
 			out.println("alert('먼저 관리자 로그인하세요');");
 			out.println("location.href='"+request.getContextPath()
-				+"/admin/login/login.do';");			
+				+"/admin/adminLogin.do';");			
 			out.println("</script>");
 			
 			return false;
